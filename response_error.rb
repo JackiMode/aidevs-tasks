554aa672a424aae46f3a5418a5586d8dbde1f1f8
Andrzej Jackewicz <jacki@idealan.pl>
2023-04-25 17:45:01 +0200

    A little refactoring

--- response_error.rb
+++ response_error.rb
@@ -1,13 +1,14 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'json'
+
 class ResponseError < StandardError
   attr_reader :uri, :res
-  def initialize(msg, res, uri, log)
+  def initialize(msg, res, uri, log, method)
     @uri = uri
     @res = res
-    super("#{msg}: #{uri} return #{res.code} #{res.message} #{res.body}")
-    log.write("#{uri} return #{res.code} #{res.message}")
-    puts "LOG: #{uri} return #{res.code} #{res.message}"
+    super("#{msg}: #{method} #{uri} return #{res.code} #{res.message} #{JSON.parse(res.body)}")
+    log.write("#{msg}: #{method} #{uri} return #{res.code} #{res.message} \"#{JSON.parse(res.body)['msg']}\"")
   end
 end
